ef5677c8fc3eedad852286b5699487c374b55c46
Gabriel Godoi <gabrielca2@hotmail.com>
2024-07-01 22:10:20 -0300

    rota de teste e cors

--- API-NODE/backend/src/server.ts
+++ API-NODE/backend/src/server.ts
@@ -1,9 +1,14 @@
 import Fastify from 'fastify';
-import cors from '@fastify/cors';
+import { routes } from './routes'
+import cors from '@fastify/cors'
 
 const app = Fastify({logger: true})
 
 const start = async () => {
+
+    await app.register(cors);
+    await app.register(routes);
+
     try{
         await app.listen({ port:3333 })
     }catch(err){
@@ -11,3 +16,5 @@ const start = async () => {
     }
 
 }
+
+start();

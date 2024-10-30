920c012e68ed5b04923ec928ffd18a637d76f61c
Gabriel Godoi <gabrielca2@hotmail.com>
2024-10-29 21:56:32 -0300

    aula 6

---
+++ Curso de JS e TP/aula6/index.js
@@ -0,0 +1,10 @@
+// let nome = "Henrique"; //String
+
+// console.log(nome, "nasceu em 1997.");
+// console.log("Em 2000", nome, "conheceu Maria em 2012.");
+// console.log("Maria teve 1 filho com", nome, "em 2015.");
+// console.log('O filho de', nome, 'se chama Eduardo.');
+
+let nome = "Gabriel";
+let sobrenome = "Godoi";
+console.log(nome + " e o sobrenome é, " + sobrenome);

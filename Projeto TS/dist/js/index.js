846351d34f9aefb097bbd41203128e24c970f1b8
Gabriel Godoi <gabrielca2@hotmail.com>
2024-10-30 21:22:36 -0300

    inicio das tipagens

--- Projeto TS/dist/js/index.js
+++ Projeto TS/dist/js/index.js
@@ -1,50 +1,35 @@
+"use strict";
 //string, number, boolean, ...
-const x: number = 25;
+const x = 25;
 console.log(x);
-
 // interferencia x annotation
 let y = 12;
 // y = "teste";
-
-let z: number = 12;
+let z = 12;
 console.log(z + y);
-
 // tipos básicos
-let firstName: string = "Gabriel";
-let age: number = 27;
-const isAdmin: boolean = true;
-
+let firstName = "Gabriel";
+let age = 27;
+const isAdmin = true;
 // String != string
-
 console.log(typeof firstName);
-
 firstName = "Godoi";
-
 console.log(firstName);
-
 // object
-const myNumbers: number[] = [1, 2, 3];
-
+const myNumbers = [1, 2, 3];
 console.log(myNumbers);
 console.log(myNumbers.length);
 console.log(firstName.toUpperCase());
 myNumbers.push(5);
-
 console.log(myNumbers);
-
 // tuplas -> tuple
-
-let myTumple: [number, string, string[]];
-
+let myTumple;
 myTumple = [5, "teste", ["a", "b"]];
-
 // myTumple = [true, false, true]
-
 // object literals -> {prop: value}
-const user: { name: string; age: number } = {
-  name: "Pedro",
-  age: 87,
+const user = {
+    name: "Pedro",
+    age: 87,
 };
-
 console.log(user);
 console.log(user.name);

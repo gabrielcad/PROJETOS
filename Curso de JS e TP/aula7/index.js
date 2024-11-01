45459b3dd62c98c5683e020073aebed4a8d70f5c
Gabriel Godoi <gabrielca2@hotmail.com>
2024-11-01 13:19:35 -0300

    introdução em constantes e diferenças ao usar let

--- Curso de JS e TP/aula7/index.js
+++ Curso de JS e TP/aula7/index.js
@@ -7,3 +7,20 @@
 // Não podemos modeficar o valor de uma constante
 // NÃO UTILIZE VAR, UTILIZE CONST.
 
+// diferente da variavel let a const não é mutavel
+// const nome = 'Godoi';
+// nome = 'Otavio';
+
+// console.log(nome);
+
+// const primeiroNumero = 5;
+// const segundoNumero = 10;
+// const resultado = primeiroNumero * segundoNumero;
+// console.log(resultado);
+
+const produtoInversor = 150;
+const produtoPainel = 300;
+const produtoEstrutura = 140;
+let descontoPainel = 25;
+const valorTotal = produtoInversor + produtoPainel + produtoEstrutura - descontoPainel;
+console.log('O valor total do gerador é de ' + '' + valorTotal);

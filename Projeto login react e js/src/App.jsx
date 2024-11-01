79cd098a0de934e284bef5657616ba774956409c
Gabriel Godoi <gabrielca2@hotmail.com>
2024-10-31 21:30:14 -0300

    inserindo estilo, fundo e logica basica para receber os dados do input e retornar em um alert

--- Projeto login react e js/src/App.jsx
+++ Projeto login react e js/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./Components/Login/Login";
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Login/>
     </div>
   );
 }
--- Projeto login react e js/src/Components/Login/Login.jsx
+++ Projeto login react e js/src/Components/Login/Login.jsx
@@ -1,19 +1,62 @@
-import { faUser, FaLock } from "react-icons/fa";
+import { FaUser, FaLock } from "react-icons/fa";
+
+import { useState } from "react";
+
+import "./Login.css";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    alert("Enviando os dados, aguarde ...");
+    alert(
+      "Seu Login é :" +
+        " " +
+        username +
+        " " +
+        " E sua senha :" +
+        " " +
+        password
+    );
+  };
+
   return (
     <div className="container">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h1>Acesse o sistema</h1>
-        <div>
-          <input type="email" placeholder="E-mail" />
-          <faUser className="icon" />
+        <div className="input-field">
+          <input
+            type="email"
+            placeholder="E-mail"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
         </div>
-        <div>
-          <input type="password" placeholder="Senha" />
-          <FaLock className="" />
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
+
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueceu a senha?</a>
         </div>
         <button>Entrar</button>
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registrar</a>
+          </p>
+        </div>
       </form>
     </div>
   );

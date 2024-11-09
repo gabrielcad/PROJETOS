93c54e4ee24d9ee622ad38ffb018850b3e9f1f3f
Gabriel Godoi <gabrielca2@hotmail.com>
2024-11-09 20:15:14 -0300

    estilizações, sidebar, profile, foto do usuário e botão para editar perfil

--- Rocketseat/React/ignite/01-fundamentos/src/App.jsx
+++ Rocketseat/React/ignite/01-fundamentos/src/App.jsx
@@ -1,22 +1,30 @@
 // JSX = JavaScript + XML (html)
 import { Header } from "./components/Header";
 import { Post } from "./Post";
-import "./styles.css";
 
+import styles from "./App.module.css";
+
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
-      <div>
-        <Header />
-        
-        <Post
-          author="Gabriel Godoi"
-          content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum nemo modi consequuntur odit repudiandae itaque ea obcaecati aspernatur aliquid, labore debitis consectetur commodi assumenda eveniet at mollitia unde vel fuga."
-        />
-        <Post
-          author="Leonardo Ribeiro"
-          content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum nemo modi consequuntur odit repudiandae itaque ea obcaecati aspernatur aliquid, labore debitis consectetur commodi assumenda eveniet at mollitia unde vel fuga."
-        />
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Gabriel Godoi"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum nemo modi consequuntur odit repudiandae itaque ea obcaecati aspernatur aliquid, labore debitis consectetur commodi assumenda eveniet at mollitia unde vel fuga."
+          />
+          <Post
+            author="Leonardo Ribeiro"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum nemo modi consequuntur odit repudiandae itaque ea obcaecati aspernatur aliquid, labore debitis consectetur commodi assumenda eveniet at mollitia unde vel fuga."
+          />
+        </main>
       </div>
+    </div>
   );
 }
--- Rocketseat/React/ignite/01-fundamentos/src/components/Header.jsx
+++ Rocketseat/React/ignite/01-fundamentos/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import "./Header.module.css";
+import styles from "./Header.module.css";
+
+import igniteLogo from "../assets/ignite-logo.svg";
 
 export function Header() {
   return (
-    <header className="{styles.header}">
-      <strong>Ignite Feed</strong>
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   );
 }

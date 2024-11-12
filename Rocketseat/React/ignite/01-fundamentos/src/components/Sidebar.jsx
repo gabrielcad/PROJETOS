69abe8324a8f8732c1fa6b88d003376bc140a0df
Gabriel Godoi <gabrielca2@hotmail.com>
2024-11-11 21:23:44 -0300

    sidebar, foto de perfil, estilo do profile, icons lib

--- Rocketseat/React/ignite/01-fundamentos/src/components/Sidebar.jsx
+++ Rocketseat/React/ignite/01-fundamentos/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "phosphor-react";
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -8,12 +10,18 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/80178149?v=4" />
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/80178149?v=4"
+        />
         <strong>Gabriel Godoi</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );

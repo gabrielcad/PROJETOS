7951fc4937110f359061ca4cf74816c5a673c1d0
Gabriel Godoi <gabrielca2@hotmail.com>
2024-11-12 22:59:05 -0300

    criando componente post

--- Rocketseat/React/ignite/01-fundamentos/src/App.jsx
+++ Rocketseat/React/ignite/01-fundamentos/src/App.jsx
@@ -1,11 +1,12 @@
 // JSX = JavaScript + XML (html)
 import { Header } from "./components/Header";
-import { Post } from "./Post";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
 
 import "./global.css";
-import { Sidebar } from "./components/Sidebar";
+
 
 export function App() {
   return (

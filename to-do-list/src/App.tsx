58b53231b9e0b140a3d8dd0d928fdc2650e4e929
Gabriel Godoi <gabrielca2@hotmail.com>
2024-12-12 23:28:32 -0300

    configurando ambiente dev e adicionando tailwind

---
+++ to-do-list/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-white font-bold">
+        LISTA DE TAREFAS
+      </h1>
+    </>
+  );
+}
+
+export default App;

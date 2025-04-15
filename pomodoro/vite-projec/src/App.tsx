75481e004ba58cc4ffc905b1036939582ff5da09
Gabriel Godoi <gabrielca2@hotmail.com>
2025-04-15 12:52:48 -0300

    iniciando pomodoro

---
+++ pomodoro/vite-projec/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export function App() {
+  return (
+    <h1>Hello World</h1>
+
+  )
+}

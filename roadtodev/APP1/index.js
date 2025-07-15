78930a53a49f51ca7ce3b96892c862c03ee4f4fd
Gabriel Godoi <gabrielca2@hotmail.com>
2025-07-15 13:34:05 -0300

    enviando e recebendo valor do input

---
+++ roadtodev/APP1/index.js
@@ -0,0 +1,9 @@
+function enviarDados() {
+
+  const inputValue = document.getElementById("meuInput").value;
+
+  let minhaVariavel = inputValue;
+
+  const outputElement = document.getElementById("listaDados").textContent = "" + inputValue;
+
+};

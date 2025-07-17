e97f11dc1744835f83b469d6cae823ed6474de3e
Gabriel Godoi <gabrielca2@hotmail.com>
2025-07-17 10:31:20 -0300

    comentando

--- roadtodev/APP1/index.js
+++ roadtodev/APP1/index.js
@@ -14,3 +14,8 @@ function enviarDados() {
 
 };
 
+// Se eu fosse resumir a diferença de SSR, SPA e SSG 
+// SSR - É o servidor que monta o front pro usuário monta o dom(html) no backend e o navegador do usuario redenreriza o front.
+// SPA - É a maquina do usuário que monta o front e informações que são sensiveis ou que dependem de uma consulta no banco são feitas por request para o backend.
+// SSG seria uma aplicação estática que não sofre alteração, é montada e mantem aquilo, não tem entrada de dados.
+// Dinamico é quando os dados sofrem alterações, tem dados no banco que podem ser alterados e etc...

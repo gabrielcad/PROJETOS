2a0a675707a208cb8acd8d020dff9f32182a2430
Gabriel Godoi <gabrielca2@hotmail.com>
2025-07-17 13:38:06 -0300

    adicionando checkbox e marcando a tarefa como feita

--- roadtodev/APP1/index.js
+++ roadtodev/APP1/index.js
@@ -1,3 +1,5 @@
+// Criar e listar tarefas
+
 function enviarDados() {
 
   const input = document.getElementById("meuInput");
@@ -12,10 +14,33 @@ function enviarDados() {
 
   input.value = "";
 
+// Preciso adicionar um checkbox no começo de cada tarefa
+
+  function inserirCheckbox(){
+    const list = document.getElementById("listaDados");
+    const check = document.createElement("li");
+    checkbox.type='checkbox';
+    checkbox.value = inputValue;
+  };
+
+
+// Depois quero marcar a linha da tarefa com um risco
+
+  const taskList = document.getElementById('taskList');
+
+li.addEventListener('click', function(event) {
+  const target = event.target;
+
+  if (target.tagName === 'LI') {
+    target.classList.toggle('completed');
+  }
+});
 };
+
+ 
 
 // Se eu fosse resumir a diferença de SSR, SPA e SSG 
 // SSR - É o servidor que monta o front pro usuário monta o dom(html) no backend e o navegador do usuario redenreriza o front.
 // SPA - É a maquina do usuário que monta o front e informações que são sensiveis ou que dependem de uma consulta no banco são feitas por request para o backend.
 // SSG seria uma aplicação estática que não sofre alteração, é montada e mantem aquilo, não tem entrada de dados.
-// Dinamico é quando os dados sofrem alterações, tem dados no banco que podem ser alterados e etc...
+// Dinamico é quando os dados sofrem alterações, tem dados no banco que podem ser alterados e etc..
